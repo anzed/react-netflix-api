98fd2e8ffccddbae7d95b16ff884e4c78dea3a0e
Mikita Zakharanka <anzed313@gmail.com>
2017-09-18 02:46:23 +0300

    Changed films list

--- src/components/films-list/FilmsList.jsx
+++ src/components/films-list/FilmsList.jsx
@@ -2,10 +2,26 @@ import React from 'react';
 import FilmItem from '../film-item/FilmItem';
 
 class FilmsList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      films: []
+    };
+  }
+
+  renderList() {
+    const films = this.state.films;
+
+    return films.length > 0 ?
+      films.map(film => <FilmItem data={film} />) :
+      <div className="no-result">No films found</div>;
+  }
+
   render() {
     return (
       <div className="films-list">
-        <FilmItem />
+        {this.renderList()}
       </div>
     );
   }
