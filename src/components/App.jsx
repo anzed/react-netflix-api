14b3c1b56a06d06ae4be2492c068935c2aef7b2e
Mikita Zakharanka <anzed313@gmail.com>
2017-10-01 16:11:42 +0300

    Removed unecessary classes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,26 +6,22 @@ import NoResult from './no-result/NoResult';
 import FilmsList from './films-list/FilmsList';
 import Footer from './footer/Footer';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="app-container">
-        <div className="content">
-          <Header />
-          <Route path="/" component={Filter} />
-          <Switch>
-            <Route exact path="/" component={NoResult} />
-            {['/film/:filmName', '/search/:searchQuery'].map((path, index) =>
-              <Route exact path={path} component={FilmsList} key={index} />
-            )}
-            <Redirect from="*" to="/" />
-          </Switch>
-          <div className="pre-footer" />
-        </div>
-        <Footer />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="app-container">
+    <div className="content">
+      <Header />
+      <Route path="/" component={Filter} />
+      <Switch>
+        <Route exact path="/" component={NoResult} />
+        {['/film/:filmName', '/search/:searchQuery'].map((path, index) =>
+          <Route exact path={path} component={FilmsList} key={index} />
+        )}
+        <Redirect from="*" to="/" />
+      </Switch>
+      <div className="pre-footer" />
+    </div>
+    <Footer />
+  </div>
+);
 
 export default App;
