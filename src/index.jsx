5e38e212d82f55521f28dcbee8816d7e984feb68
Mikita Zakharanka <anzed313@gmail.com>
2017-10-01 16:07:57 +0300

    Added store

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 import App from './components/App';
+
+const store = configureStore();
 
 const render = () => {
   ReactDOM.render(
-    <Router>
-      <Route path="/" component={App} />
-    </Router>,
+    <Provider store={store}>
+      <Router>
+        <Route path="/" component={App} />
+      </Router>
+    </Provider>,
     document.getElementById('root')
   );
 };
